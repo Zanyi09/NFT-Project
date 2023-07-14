2967bfa6924519c046cda8aaf9d35ecaedeba090
Zanyi09 <nzanyi09@gmail.com>
2023-07-14 12:34:14 +0700

    update

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.alchemyapi.io/v2/Fqi__ZKwAcWOMRE6wm9rL-YegpCHJKGU",
+      accounts: [ "ad52d06588ee1ff59bada905842321b24c8a3efbc63c63d25db51959ea15c83d" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
